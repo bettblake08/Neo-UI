47270221ca28fd4b6507f1d211e5cd3a8fb541c2
bettblake08 <bettblake08@hotmail.com>
2020-03-21 23:54:49 +0300

    fix(webpack): fix webpack file loader
    - configure publicPath as file loader option

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,18 @@
 module.exports = async ({ config, mode }) => {
-    // console.dir(config.plugins, { depth: null });
-    config.module.rules.push({
-        test: /\.stories\.jsx?$/,
-        loaders: [require.resolve('@storybook/addon-storysource/loader')],
-        enforce: 'pre',
+  if (mode === "PRODUCTION") {
+    config.module.rules.map(rule => {
+      if (/file-loader/.test(rule.loader)) {
+        rule.query.publicPath = url => `../../${url}`;
+      }
+      return rule;
     });
+  }
+  config.module.rules.push({
+    test: /\.stories\.jsx?$/,
+    loaders: [require.resolve("@storybook/addon-storysource/loader")],
+    enforce: "pre"
+  });
 
-    return {
-        ...config
-    };
+//   console.dir(config.module, { depth: null });
+  return config;
 };
